e8857f8c9211dd92bc17aac2bb04f2265e43b172
Matthew Roberts <matt.roberts4000@gmail.com>
2017-08-31 19:45:46 +0100

    Added?

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Goals} = require('./models/goals');
@@ -22,6 +23,37 @@ app.post('/goals', (req, res) => {
 
 });
 
+app.get('/goals', (req, res) => {
+  Goals.find().then((goals) => {
+    res.send({ goals });
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/goals/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Goals.findById(id).then((goal) => {
+    if(!goal) {
+      return res.status(404).send();
+    }
+    res.send({goal});
+  }).catch((e) => {
+    return res.status(400).send();
+  })
+
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
+});
+
+describe('GET /goals/:id', () => {
+  it('should return goal doc', (done) => {
+    
+  });
 });
